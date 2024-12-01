bb91ab8b414d4a0e9e43288b3ca4c1531276c5c2
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-12-01 07:21:51 +0200

    add template

---
+++ template/script.py
@@ -0,0 +1,21 @@
+#
+
+
+def first_challenge(filename):
+    for line in lines:
+        pass
+
+    return 0
+
+
+def second_challenge(filename):
+    for line in lines:
+        pass
+
+    return 0
+
+
+if __name__ == "__main__":
+    with open("example") as f:
+        lines = f.read().splitlines()
+    print(first_challenge(lines))
